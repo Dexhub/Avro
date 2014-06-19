2fb9ef5f3304fdeae31968d8215f6e05804f6a34
Himanshu Shah <himanshu1410@live.com>
2014-06-19 14:29:54 -0400

    updated run.sh for example code

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #gcc -g quickstop.c  -I /home/dexter/Syncsort/avro-apache-git/lang/c/src -L /home/dexter/Syncsort/avro-apache-git/lang/c/build/avrolib/lib/static/ -lavro -o quickstop
-gcc -g sample.c  -I /home/dexter/Syncsort/dmx/file-util/src -L /home/dexter/Syncsort/dmx/file-util/build/avrolib/lib/static/ -lavro -o sample 
+#gcc -g sample.c  -I /home/dexter/Syncsort/dmx/file-util/src -L /home/dexter/Syncsort/dmx/file-util/build/avrolib/lib/static/ -lavro -o sample 
 gcc -g example.c avro-lib.c  -I /home/dexter/Syncsort/dmx/file-util/src -L /home/dexter/Syncsort/dmx/file-util/build/avrolib/lib/static/ -lavro -o example
 
 #gcc -g sample.c ggets.c -I home/dexter/Syncsort/avro/lang/c/src -L /home/dexter/Syncsort/dmx/file-util/build/avrolib/lib/static/ -lavro -o sample
 #./sample data.avsc
+echo "### Testing example code. Output: avro.data\n"
+./example 2&> avro.data
+echo "### Converting the output to json\n "
+echo "\n\n"
+java -jar ./avro-tools-1.7.6.jar tojson data
